44a2771e9b0cbc63fd5860cb986d0a22d9fcb512
Jason Lo <lcmjlo@gmail.com>
2023-04-13 21:46:05 +0000

    add data module

---
+++ askem/data.py
@@ -0,0 +1,21 @@
+import random
+from datasets import load_dataset
+
+
+def get_covid_qa():
+    """Get the COVID-QA dataset."""
+    dataset = load_dataset("covid_qa")
+    return dataset["train"]
+
+
+COVID_QA = get_covid_qa()
+
+
+def get_example(dataset, id=None):
+    """Get an example from a dataset."""
+
+    if id is None:
+        id = random.randint(0, len(dataset))
+    data = dataset[id]
+    columns = ["context", "question", "is_impossible", "answers"]
+    return {x: data[x] for x in columns}
