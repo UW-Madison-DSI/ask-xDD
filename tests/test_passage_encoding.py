3a58b71bd3a7feae6a134c199f60b1212a9fcf65
Jason Lo <lcmjlo@gmail.com>
2023-05-09 12:54:40 +0000

    update to facebook dpr

--- tests/test_passage_encoding.py
+++ tests/test_passage_encoding.py
@@ -4,7 +4,8 @@ from transformers import DPRContextEncoder, DPRContextEncoderTokenizer
 
 def get_passage_embedding_from_source(text: str) -> torch.Tensor:
     """Get the passage embedding from the HuggingFace source."""
-    model_name = "vblagoje/dpr-ctx_encoder-single-lfqa-wiki"
+    # model_name = "vblagoje/dpr-ctx_encoder-single-lfqa-wiki"
+    model_name = "facebook/dpr-ctx_encoder-single-nq-base"
     tokenizer = DPRContextEncoderTokenizer.from_pretrained(model_name)
     model = DPRContextEncoder.from_pretrained(model_name)
     input_ids = tokenizer(text, return_tensors="pt")["input_ids"]
