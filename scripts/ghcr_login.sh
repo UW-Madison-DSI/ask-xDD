74a4185987f9b0c37d219403a3f5b2ddb922a8c9
Jason Lo <lcmjlo@gmail.com>
2023-05-11 22:07:29 +0000

    add util scripts

--- scripts/ghcr_login.sh
+++ scripts/ghcr_login.sh
@@ -1,1 +1,2 @@
-echo $GHCR_TOKEN | docker login ghcr.io -u JasonLo --password-stdin
+#!/bin/bash
+export $(grep -v '^#' .env | xargs -d '\n') && docker login ghcr.io -u JasonLo -p $GHCR_TOKEN
---
+++ scripts/push_chtc_container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker push ghcr.io/jasonlo/askem-chtc
