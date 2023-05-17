c90c682b1feb71929f4d4133bbc2461aa2f220f0
Jason Lo <lcmjlo@gmail.com>
2023-05-17 16:25:47 +0000

    small env related QoL improvements

--- scripts/export_env.sh
+++ scripts/export_env.sh
@@ -1,1 +1,3 @@
+# Load .env into memory as environment variable
+
 export $(grep -v '^#' .env | xargs -d '\n')
---
+++ scripts/hf_cache_location.sh
@@ -0,0 +1,4 @@
+export TRANSFORMERS_CACHE=/data/clo36/huggingface/models
+export HF_DATASETS_CACHE=/data/clo36/huggingface/datasets
+export HF_MODULES_CACHE=/data/clo36/huggingface/modules
+export HF_METRICS_CACHE=/data/clo36/huggingface/metrics
