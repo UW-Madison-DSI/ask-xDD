b5355bae9affeed6a50c3a9e466375fb37629356
iaross <iaross@cosmos0001.chtc.wisc.edu>
2023-06-20 20:12:22 -0500

    add COSMOS image

--- askem/demo/style.py
+++ askem/demo/style.py
@@ -1,5 +1,7 @@
 import os
+import requests
 from typing import Optional
+from base64 import b64decode
 from askem.preprocessing import WEAVIATE_DOC_TYPES
 
 COSMOS_URL = os.getenv("COSMOS_URL")
@@ -14,6 +16,15 @@ def to_url(cosmos_object_id: str) -> str:
     """Convert cosmos object id to URL."""
     return f"{COSMOS_URL}/{cosmos_object_id}"
 
+
+def get_image_bytes(cosmos_object_id: str ) -> str:
+    resp = requests.get(to_url(cosmos_object_id))
+    result = resp.json()
+    try:
+        img_bytes = result["success"]["data"][0]["properties"]["image"]
+        return img_bytes
+    except:
+        return None
 
 def to_html(
     doc_type: str,
@@ -37,5 +48,9 @@ def to_html(
     if doc_type in ["figure", "table"]:
         html += "<br>"
         html += f"<a href='{to_url(cosmos_object_id)}'>Link</a>"
+        img = get_image_bytes(cosmos_object_id)
+        if img is not None:
+            html += "<br>"
+            html += f"<img src='data:image/jpg;base64,{get_image_bytes(cosmos_object_id)}' />"
 
     return html
