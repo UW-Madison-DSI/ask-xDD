861cd917e4ca00be820aa28545aaf0c29392f100
Jason Lo <lcmjlo@gmail.com>
2023-09-19 20:43:57 +0000

    draft v2 patch

--- askem/retriever/base.py
+++ askem/retriever/base.py
@@ -19,44 +19,9 @@ def get_client(url: str = None, apikey: str = None) -> weaviate.Client:
     return weaviate.Client(url, weaviate.auth.AuthApiKey(apikey))
 
 
-def to_v2(schema: dict) -> dict:
-    """Convert a v1 schema to a v2 schema."""
-    v2_extra_properties = []
-
-    # 10 new paper terms
-    for i in range(10):
-        v2_extra_properties.append(
-            {
-                "name": f"paper_terms_{i}",
-                "dataType": ["text"],
-                "moduleConfig": {"text2vec-transformers": {"skip": True}},
-            }
-        )
-
-    # 3 new paragraph terms
-    for i in range(3):
-        v2_extra_properties.append(
-            {
-                "name": f"paragraph_terms_{i}",
-                "dataType": ["text"],
-                "moduleConfig": {"text2vec-transformers": {"skip": True}},
-            }
-        )
-
-    schema["class"] = "PassageV2"
-    schema["properties"].extend(v2_extra_properties)
-    return schema
-
-
-def init_retriever(client=None, version: int = 1) -> None:
-    """Initialize the passage retriever."""
-
-    if client is None:
-        client = get_client()
-
-    # Passage schema, for all types of documents, including paragraph, figures and tables
-    # TODO: If safe, rename to document?
-    PASSAGE_SCHEMA = {
+def get_v1_schema() -> dict:
+    """Obtain the v1 schema."""
+    return {
         "class": "Passage",
         "description": "Paragraph chunk of a document",
         "vectorizer": "text2vec-transformers",
@@ -92,9 +57,50 @@ def init_retriever(client=None, version: int = 1) -> None:
         ],
     }
 
-    # V2 schema diff
-    if version == 2:
-        PASSAGE_SCHEMA = to_v2(PASSAGE_SCHEMA)
+
+def to_v2(schema: dict) -> dict:
+    """Convert a v1 schema to a v2 schema."""
+    v2_extra_properties = []
+
+    # 10 new paper terms
+    for i in range(10):
+        v2_extra_properties.append(
+            {
+                "name": f"article_terms_{i}",
+                "dataType": ["text"],
+                "moduleConfig": {"text2vec-transformers": {"skip": True}},
+            }
+        )
+
+    # 3 new paragraph terms
+    for i in range(3):
+        v2_extra_properties.append(
+            {
+                "name": f"paragraph_terms_{i}",
+                "dataType": ["text"],
+                "moduleConfig": {"text2vec-transformers": {"skip": True}},
+            }
+        )
+
+    schema["properties"].extend(v2_extra_properties)
+    return schema
+
+
+def get_v2_schema() -> dict:
+    """Obtain the v2 schema."""
+    return to_v2(get_v1_schema())
+
+
+def init_retriever(client=None, version: int = 1) -> None:
+    """Initialize the passage retriever."""
+
+    if client is None:
+        client = get_client()
+
+    if version == 1:
+        PASSAGE_SCHEMA = get_v1_schema()
+    elif version == 2:
+        PASSAGE_SCHEMA = get_v2_schema()
 
     client.schema.create_class(PASSAGE_SCHEMA)
 
