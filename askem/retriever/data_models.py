b1084f30b7c3ba23d67fffaccac95b9ec8609ed3
Jason Lo <lcmjlo@gmail.com>
2023-10-02 19:33:37 +0000

    Hybrid search integration #76

--- askem/retriever/data_models.py
+++ askem/retriever/data_models.py
@@ -24,11 +24,16 @@ class Query(BaseModel):
     question: str
     top_k: int = 5
     distance: float = 0.5
+
+    # Where per-filters
     topic: Optional[str] = None
     doc_type: Optional[str] = None
     preprocessor_id: Optional[str] = None
     article_terms: Optional[List[str]] = None
     paragraph_terms: Optional[List[str]] = None
+    paper_ids: Optional[List[str]] = None
+
+    # Search vectoring
     move_to: Optional[str] = None
     move_to_weight: Optional[float] = None
     move_away_from: Optional[str] = None
