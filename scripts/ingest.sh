8188ad0b85322d2ff7015d98fe82f27b02dccedb
Jason Lo <lcmjlo@gmail.com>
2024-02-12 09:45:18 -0600

    add date to log for cron

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+current_date=$(date +"%Y-%m-%d")
+
 # This will resume from ./tmp/id2topics.pkl
 # nohup python ./askem/ingest_v2.py --resume &> ingest.log &
 
 # This will start from scratch
-nohup python ./askem/ingest_v2.py &> ingest.log &
+nohup python ./askem/ingest_v2.py &> "tmp/ingest_$current_date.log" &
