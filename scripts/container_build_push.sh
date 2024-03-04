b3340e96ad1428262627741639c0c3ff4d4486be
Jason Lo <lcmjlo@gmail.com>
2024-03-04 20:38:50 +0000

    remove trash

--- scripts/container_build_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the container
-docker build . -f chtc/Dockerfile -t ghcr.io/jasonlo/askem-chtc
-
-# Push the container
-docker push ghcr.io/jasonlo/askem-chtc
--- scripts/ghcr_login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get secrets from .env
-export $(grep -v '^#' .env | xargs -d '\n')
-
-# Login to GitHub Container Registry
-docker login ghcr.io -u JasonLo -p $GHCR_TOKEN
